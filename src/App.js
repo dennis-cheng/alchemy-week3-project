befb2232b475db6c97405919cb8283be3c408c1a
Dennis Cheng <dennischeng@hotmail.co.za>
2023-06-11 22:01:35 +0200

    Adds rendering of transactions of the later block

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
+import { Alchemy, Network, Utils } from "alchemy-sdk"
+import { useEffect, useState, Fragment } from "react"
 
-import './App.css';
+import "./App.css"
 
 // Refer to the README doc for more information about using API
 // keys in client-side code. You should never do this in production
@@ -9,28 +9,53 @@ import './App.css';
 const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
-};
-
+}
 
 // In this week's lessons we used ethers.js. Here we are using the
 // Alchemy SDK is an umbrella library with several different packages.
 //
 // You can read more about the packages here:
 //   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
-const alchemy = new Alchemy(settings);
+const alchemy = new Alchemy(settings)
 
 function App() {
-  const [blockNumber, setBlockNumber] = useState();
+  const [blockNumber, setBlockNumber] = useState()
+  const [transactions, setTransactions] = useState([])
 
   useEffect(() => {
     async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+      setBlockNumber(await alchemy.core.getBlockNumber())
     }
 
-    getBlockNumber();
-  });
+    getBlockNumber()
+  })
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  useEffect(() => {
+    getTransactions()
+  }, [blockNumber])
+
+  const getTransactions = async () => {
+    const block = await alchemy.core.getBlockWithTransactions(blockNumber)
+    setTransactions(block.transactions)
+  }
+
+  return (
+    <div>
+      <div className="App">Block Number: {blockNumber}</div>
+      <div
+        style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "8px" }}
+      >
+        <div>Tx Hash</div>
+        <div>Value</div>
+        {transactions.map((t) => (
+          <Fragment key={t.hash}>
+            <div>{t.hash}</div>
+            <div>{Utils.formatEther(t.value.toString())} ETH</div>
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
